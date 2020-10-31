8b0db03be7c60d898b90c987ab9f10494da74bc4
carlos_paniagua <carlos.paniagua042137@gmail.com>
2020-10-31 13:04:16 +0900

    了

---
+++ HTML/templates/static/main.js
@@ -0,0 +1,6 @@
+// h1要素を取得
+const h1 = document.querySelector("h1");
+
+h1.addEventListener("click", () => {
+  h1.textContent = "Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!Changed!";
+});
---
+++ HTML/templates/static/speak.js
@@ -0,0 +1,32 @@
+const startBtn = document.querySelector('#start-btn');
+const stopBtn = document.querySelector('#stop-btn');
+const resultDiv = document.querySelector('#result-div');
+
+SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+let recognition = new SpeechRecognition();
+
+recognition.lang = 'ja-JP';
+recognition.interimResults = true;
+recognition.continuous = true;
+
+let finalTranscript = ''; // 確定した(黒の)認識結果
+
+recognition.onresult = (event) => {
+  let interimTranscript = ''; // 暫定(灰色)の認識結果
+  for (let i = event.resultIndex; i < event.results.length; i++) {
+    let transcript = event.results[i][0].transcript;
+    if (event.results[i].isFinal) {
+      finalTranscript += transcript;
+    } else {
+      interimTranscript = transcript;
+    }
+  }
+  resultDiv.innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</i>';
+}
+
+startBtn.onclick = () => {
+  recognition.start();
+}
+stopBtn.onclick = () => {
+  recognition.stop();
+}
