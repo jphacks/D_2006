4549e0218812bab847d3b4b5c16284dba90177e3
carlos_paniagua <carlos.paniagua042137@gmail.com>
2020-11-02 18:47:51 +0900

    無理

--- server/static/speak.js
+++ server/static/speak.js
@@ -9,10 +9,11 @@ recognition.interimResults = true;
 recognition.continuous = true;
 
 let finalTranscript = ''; // 確定した(黒の)認識結果
-
+const div_arr=["です","ます","である","だ","ました"]
 recognition.onresult = (event) => {
+  
   var resultDiv = document.querySelectorAll('.user_say');
-  resultDiv=resultDiv[resultDiv.length-1];
+  resultDiv = resultDiv[resultDiv.length - 1];
   let interimTranscript = ''; // 暫定(灰色)の認識結果
   for (let i = event.resultIndex; i < event.results.length; i++) {
     let transcript = event.results[i][0].transcript;
@@ -22,8 +23,19 @@ recognition.onresult = (event) => {
       interimTranscript = transcript;
     }
   }
-
-  resultDiv.innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</i>';
+  let flg=false;
+  for(let s of div_arr){
+    if(finalTranscript.includes(s)){
+      flg=true;
+      resultDiv.innerHTML = finalTranscript+"。";
+      make_new_user_say();
+      finalTranscript='';
+    }
+  }
+    // if(finalTranscript.includes("です"))
+  if(!flg){
+     resultDiv.innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</i>';
+  }
 }
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
@@ -35,10 +47,7 @@ function rec_start() {
 const SEND_STATE_NOTSEND = "not_send"
 const SEND_STATE_SENT = "sent"
 
-
-startBtn.onclick = () => {
-
-  rec_start();
+function make_new_user_say(){
   var elem2 = document.getElementById("make-result");
   elem2.insertAdjacentHTML("beforeend", `
     <div class="bms_message bms_right">
@@ -50,6 +59,28 @@ startBtn.onclick = () => {
     </div>
     <div class="bms_clear"></div>
   `);
+
+}
+
+startBtn.onclick = () => {
+  finalTranscript='';
+  rec_start();
+  // var elem2 = document.getElementById("make-result");
+  // elem2.insertAdjacentHTML("beforeend", `
+  //   <div class="bms_message bms_right">
+  //     <div class="bms_message_box">
+  //       <div class="bms_message_content">
+  //         <div class="bms_message_text user_say" Id="result-div" value="not_send"></div>
+  //       </div>
+  //     </div>
+  //   </div>
+  //   <div class="bms_clear"></div>
+  // `);
+  make_new_user_say();
+
+
+
+
 
   var elem = document.getElementById("make-stop-btn");
   elem.innerHTML = `
@@ -83,6 +114,19 @@ function stop_rec() {
   recognition.stop();
 }
 
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+
+  if (keyName === ',') {
+    punctuation += "、";
+    return;
+  }
+  if (keyName === '.') {
+    punctuation += "。\n";
+    return;
+  }
+}, false);
+
 // // 送信ボタン
 // let send_btn=document.querySelector("#bms_send_btn");
 // 送信ボタンはクリックされると、JSON形式でUserがしゃべった言葉をサーバへ送信する処理が行われる
@@ -111,14 +155,44 @@ send_btn.onclick = () => {
   //   } ) );
 
   // await promiss();
-  for(let element of ele){
-          if (element.getAttribute("value") === SEND_STATE_NOTSEND) {
-            servertext += element.textContent;
-            console.log(element.textContent);
-            element.setAttribute("value", SEND_STATE_SENT);
-      }
+  for (let element of ele) {
+    if (element.getAttribute("value") === SEND_STATE_NOTSEND) {
+      servertext += element.textContent;
+      console.log(element.textContent);
+      element.setAttribute("value", SEND_STATE_SENT);
+    }
   }
-  if(servertext==="")return;
+  
+
+
+
+
+  if (servertext === "") {
+    // なにも話していない
+    return;
+  }
+
+  let first_element=ele[0];
+  let idx=0;
+  while(servertext.length>10){
+    // search div
+    let f=0;
+    for(let s of div_arr){
+      f=servertext.indexOf(s,idx);
+      if(f!=-1){
+        break;
+      }
+    }
+    idx=f;
+    innert_text=servertext.substring(0,idx);
+    servertext=servertext.substring(idx,servertext.length);
+    // element 代入
+   
+    
+  }
+
+
+
   const obj = { "anal_text": servertext };
   const method = "POST";
   const body = JSON.stringify(obj);
